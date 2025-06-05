b55aeda3723d13bf61ba529a1f31c88790ee7b6a
Liam Gessman <Liamngessman+frc@gmail.com>
2025-06-05 13:03:21 -0400

    cool background+unrounded nav bar

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,8 +3,9 @@ import Link from 'next/link'
 export default function ContactPage() {
   return (
     <CursorGlow>
+      <div className="main"></div>
       <div className="min-h-screen p-8 pb-20 sm:p-20">
-        <header className="fixed top-0 left-0 w-full px-6 py-1 bg-white text-black shadow-md z-50 flex rounded-full items-center justify-center">
+        <header className="fixed top-0 left-0 w-full px-6 py-1 bg-white text-black shadow-md z-50 flex items-center justify-center">
           <nav className="bg-white text-black p-4 flex gap-4 text-lg">
             <Link href="/" className="hover:underline">Home</Link>
             <Link href="/about" className="hover:underline">About</Link>
--- src/app/sponsers/page.tsx
+++ src/app/sponsers/page.tsx
@@ -4,8 +4,9 @@ import Image from 'next/image';
 export default function SponsorsPage() {
   return (
     <CursorGlow>
+      <div className="main"></div>
       <div className="min-h-screen p-8 pb-20 sm:p-20">
-        <header className="fixed top-0 left-0 w-full px-6 py-1 bg-white text-black shadow-md z-50 flex rounded-full items-center justify-center">
+        <header className="fixed top-0 left-0 w-full px-6 py-1 bg-white text-black shadow-md z-50 flex items-center justify-center">
           <nav className="bg-white text-black p-4 flex gap-4 text-lg">
             <Link href="/" className="hover:underline">Home</Link>
             <Link href="/about" className="hover:underline">About</Link>
