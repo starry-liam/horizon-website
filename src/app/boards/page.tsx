bad49253863c7ccdbf18145d8ee6918a1a8b0c8f
Liam Gessman <Liamngessman+frc@gmail.com>
2025-06-18 18:28:13 -0400

    Added V1 to boards page need to fix position of images and desquash the text

--- src/app/boards/page.tsx
+++ src/app/boards/page.tsx
@@ -15,6 +15,29 @@ export default function BoardsPage() {
             <Link href="/sponsers" className="hover:underline">Sponsors</Link>
           </nav>
         </header>
+        <main className="pt-32 max-w-3xl mx-auto space-y-8 text-center">
+          <div className='space-y-4 text-lg text-gray-200 justify-start flex items-center gap-200'>
+            <img 
+              src="/Front.png"
+              alt="Horizon V1 Front"
+              width={300}
+              height={50}
+              className="dark relative z-10 glow"
+            />
+            <div className='space-y-4 text-lg text-gray-200 justify-start items-center'>
+              <p className='text-3xl font-bold'>Horizon V1</p>
+              <p className='text-lg font-bold'>The first version of the Horizon Avionics board.</p>
+              <p>We are still in development! If you would like to help, Please consider a donation on the sponsers page, Or contact us using the information on the contact page.</p>
+            </div>
+            <img 
+              src="/Back.png"
+              alt="Horizon V1 Back"
+              width={300}
+              height={500}
+              className="dark relative z-10 glow"
+            />
+          </div>
+        </main>
 
       </div>
     </CursorGlow>
